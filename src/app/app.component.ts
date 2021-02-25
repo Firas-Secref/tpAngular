068a9b86967e1627a68726e3a46be898c516f2cb
firas secref <sh.firas10@gmail.com>
2021-02-25 17:24:50 +0100

    second commit tp2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TPAngular';
+  url = 'https://img.20mn.fr/4h6nRiywT-K3L4xrdTrjjA/830x532_simulation-informatique-disque-accretion-autour-horizon-trou-noir.jpg';
+
+
+  // tslint:disable-next-line:typedef
+  onSave($event: MouseEvent) {
+    console.log('evenement', $event);
+  }
 }
